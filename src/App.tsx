b2b5b1fab1f9330b11f215863ed24377e8c45247
Likhon-25 <likhonmondol327@gmail.com>
2026-08-31 01:39:00 +0600

    Learned  How to pass and read Props in React

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,30 @@ function App() {
   return (
     <>    
           <h1>Assalamulaikum</h1>  
-          <Student></Student>
-          <Student></Student>
+          {/* <Student name="Subida Badi" gpa='3.65'></Student>
+          <Student name="Pati leader" ></Student> */}
           {/* <Person></Person> 
           <Gadgets></Gadgets>        */}
-
+          <Developer language ='TypeScript' exprience = '3 years'></Developer>
+          <Developer language ='Python' exprience = '13 years'></Developer>
     </>
   )
 }
-function Student(){
+
+function Developer(props){
+  console.log(props);
+  return(
+    <div className="student">
+      <h4>Programming Language: {props.language} </h4>
+      <p>Years of Exprience : {props.exprience}</p>
+    </div>
+  )
+}
+
+
+function Student(props){
+  console.log('Inside the student component', props);
+  console.log(props.name);
   const studentStyle = {
     border: '2px solid red',
     margin: '5px',
@@ -22,7 +37,7 @@ function Student(){
     <div style={{ border: '2px solid red',
     margin: '15px',
     borderRadius: '5px'}}>
-      <h3>Name: </h3>
+      <h3>Name:{props.name} </h3>
       <p>Grade: </p>
     </div>
   )
