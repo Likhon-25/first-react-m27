1340529c6c392cd698aa012c610d8c0dd923edbe
Likhon-25 <likhonmondol327@gmail.com>
2026-08-30 23:05:09 +0600

    Start Learnd React.js

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+function App() {
+
+  return (
+    <>    
+          <h1>Assalamulaikum</h1>  
+          {/* <Person></Person> 
+          <Gadgets></Gadgets>        */}
+
+    </>
+  )
+}
+function 
+
+
+// function Person(){
+//   return <p>I'm Back.</p>
+// }
+// function Gadgets(){
+//   const food = "Chocolate"
+//   return (
+//     <>
+//     <p>I'm Likhon</p>
+//     <p>I'm {10 + 10} years old</p>
+//     <p>I read in Diploma in Engineering</p>
+//     <p>Favourite Food {food}</p>
+//     </>
+//   )
+// }
+
+export default App
